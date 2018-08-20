d09d112bc02bd1ff24eeac05caf42775503e4433
高婷婷 <>
2018-08-20 17:31:23 +0800

    123

---
+++ InKeLive-gcz_99/InKeLive/Main/动态/VC/SendDynamicViewController.m
@@ -0,0 +1,37 @@
+//
+//  SendDynamicViewController.m
+//  InKeLive
+//
+//  Created by 高婷婷 on 2018/8/20.
+//  Copyright © 2018年 jh. All rights reserved.
+//
+
+#import "SendDynamicViewController.h"
+
+@interface SendDynamicViewController ()
+
+@end
+
+@implementation SendDynamicViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
